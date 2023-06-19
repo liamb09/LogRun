a384286ccd6f51d544195eafd5cf3d465e8b0a9c
Liam <85259706+liamb09@users.noreply.github.com>
2023-06-19 11:43:42 -0500

    Add files via upload

--- log.php
+++ log.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+$lrdistance = "";
+$total_dist = 0;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $lrdistance = $_POST["record-run-distance"];
+    $total_dist += $lrdistance;
+}
+?>
 
 <html>
     <head>
@@ -15,6 +22,11 @@
     </header>
     <div id="main-image">
         <img src="https://lh3.googleusercontent.com/xJPHEtkppi-rVBXHBre0K4TcjpvpLZgPU4egPVSEhOiBP9UqVIsjKk9sOV_vH0brtn9ptAAK2Ihz95FFIcjkL5Ub2iXNAzNIqETjV6xyPzQnU9uu9cGVM2x0kzp0u6qj2wGATAxB0byimejvEVCGkWBO9rlq7reqOe8euLToBE544F6WZ2CWTu_zoAQpTwHPNsgVo7AbzKCtxsLpijb7TjJLYvw75e-5Hf8Xl53EVyjtBRzGWStnmrhuTt40BCmN-DcgUahcwDlsc-2RiN6V-zDDzG33LiHjCFIwaThKQdEktEhw_D3tBPaFBDf4xfWl9KqBLqNfpPirk58BhU6T6egWhibDw6h7v4S_frx5H08mSW7dNw8zzngXg0ND_yu13hYZ3BhTSV9p4-UZVrdSgcTB5x92Y280ruZ4j3faJbJOm81tbpt4sqhDnzn_N-wApxdDnyqas-yefje-z-KZS9KlXsEX4CTY-yVmFWKA_s1ld7d9yRWEM76gs02Y8C1EIQgSSXR9JWMsQlYbp1W-TdDgXsAOEimGltHr7CdOG6FchHJMfWfRbXcQnV2iVL-KArSEht9f0Nkg2v_qW5MIoYtxWwtC0m7RvEt2sq4S1mn2E4dHt_6egyNAhd9qwvW3l-HgAcJyNq49oQ8b-murlO5KOsy0s0xKI3wBou9-pbrCK5RGoOPwLeRbvqsb2aktiu7-wsw8g_D6h2v-U6eJwS8o0XLh9o5oMct6O1AFldalfiT3qQ95gUJxaOQRF5t6mnpRxqbYvLFklI75XfxsX1QhlevTBc9rcU9z-Bje8xxKYJZDyZW9U9MrSJtXicj90l8UrmUC9Xxlb9UPXKGyJPq0vFRFTL6Fa2fCLemRttd0qPSYB2i2krLno44tEKXuMtIavHRkSZQbbAmVXu5aIYc8i8oh7_ziDvPbY1RnKSYGY62h=w1080-h268-s-no?authuser=0" alt="road" class="road">
-        <h1 class="page-title">About</h1>
+        <h1 class="page-title">Running Log</h1>
     </div>
+    <br><br>
+    <h1 id="mile-count"><?=$total_dist?> Total Miles</h1>
+    <br>
+    <div id="record-run-button-container"><a href="addrun.php"><button class="button">Record Run</button></a></div>
+    <p>Your last run was <?=$lrdistance?>mi</p>
 </html>
--- scripts.js
+++ scripts.js
@@ -1,16 +1,42 @@
 function enableOtherTextbox () {
     if (document.getElementById("run-type-selector").value == "other") {
-        document.getElementById("record-run-other-textbox").removeAttribute("disabled")
+        document.getElementById("record-run-other-textbox").disabled = false
     } else {
         document.getElementById("record-run-other-textbox").value = ""
-        document.getElementById("record-run-other-textbox").setAttribute("disabled", true);
+        document.getElementById("record-run-other-textbox").disabled = true
     }
 }
 
-function disableColorInput () {
-    if (document.getElementById("record-run-color-checkbox").checked) {
-        document.getElementById("record-run-color-input").disabled = false
-    } else {
-        document.getElementById("record-run-color-input").disabled = true
+function disableColorInput (type) {
+    let checkboxid
+    let colorinputid
+    if (type == "back") {
+        checkboxid = "record-run-color-checkbox"
+        colorinputid = "record-run-color-input"
+    } else if (type == "text") {
+        checkboxid = "record-run-text-color-checkbox"
+        colorinputid = "record-run-text-color-input"
     }
+    if (document.getElementById(checkboxid).checked) {
+        document.getElementById(colorinputid).disabled = false
+        document.getElementById(colorinputid).style.display = "inline"
+        if (type == "back") {
+            document.getElementById("example-text").style.backgroundColor = document.getElementById("record-run-color-input").value
+        } else if (type == "text") {
+            document.getElementById("example-text").style.color = document.getElementById("record-run-text-color-input").value
+        }
+    } else {
+        document.getElementById(colorinputid).disabled = true
+        document.getElementById(colorinputid).style.display = "none"
+        if (type == "back") {
+            document.getElementById("example-text").style.backgroundColor = "transparent"
+        } else if (type == "text") {
+            document.getElementById("example-text").style.color = "#000000"
+        }
+    }
+}
+
+function updateExampleText () {
+    document.getElementById("example-text").style.backgroundColor = document.getElementById("record-run-color-input").value
+    document.getElementById("example-text").style.color = document.getElementById("record-run-text-color-input").value
 }
